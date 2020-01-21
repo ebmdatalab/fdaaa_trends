db80e2fe807aee44deb7127147d322011bbd2c54
Seb Bacon <seb.bacon@gmail.com>
2020-01-21 14:12:25 +0000

    Run tests better:
    * Use the same PYTHONPATH as when running a notebook
    * Use a sanitize file to ignore common false positives in diffed cell
      outputs
    * Skip checking the output of plotly chart cells; just execute them

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@
 # This awkward invocation is to get around the case where no tests are
 # found, which has exit code of 5 in pytest
 
-py.test --nbval notebooks; ret=$?; [ $ret = 5 ] && exit 0 || exit $ret
+PYTHONPATH=$(pwd) python -m pytest --sanitize-with config/nbval_sanitize_file.conf --nbval notebooks; ret=$?; [ $ret = 5 ] && exit 0 || exit $ret

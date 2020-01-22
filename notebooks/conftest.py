7643348d2c3f1f56a7b9328fa661cd0ea2405fbe
Seb Bacon <seb.bacon@gmail.com>
2020-01-22 16:52:28 +0000

    Skip stderr when comparing cell outputs in tests

--- notebooks/conftest.py
+++ notebooks/conftest.py
@@ -1,8 +1,9 @@
 # This is a pytest config file
-# https://docs.pytest.org/en/2.7.3/plugins.html?
+# https://docs.pytest.org/en/2.7.3/plugins.html
 
-# It allows us to tell nbval to skip comparing notebook outputs for
-# certain mimetypes.
+# It allows us to tell nbval (the py.text plugin we use to run
+# notebooks and check their output is unchanged) to skip comparing
+# notebook outputs for particular mimetypes.
 
 
 def pytest_collectstart(collector):
@@ -11,7 +12,7 @@ def pytest_collectstart(collector):
         and collector.fspath.ext == ".ipynb"
         and hasattr(collector, "skip_compare")
     ):
-        # Skip plotly comparison because something to do with
+        # Skip plotly comparison, because something to do with
         # responsive plot sizing makes output different in test
         # environment
-        collector.skip_compare += ("application/vnd.plotly.v1+json",)
+        collector.skip_compare += ("application/vnd.plotly.v1+json", "stderr")
